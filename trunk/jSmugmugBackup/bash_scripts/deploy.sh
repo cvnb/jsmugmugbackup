60fa42a6358db3b8c67072676f18f230f47fce1a
chradam <chradam@5eeee95c-e655-0410-a769-073df2a2cce5>
2008-11-09 14:50:34 +0000

    deployment script now includes license info and create an archive

--- trunk/jSmugmugBackup/bash_scripts/deploy.sh
+++ trunk/jSmugmugBackup/bash_scripts/deploy.sh
@@ -45,6 +45,15 @@ cd deployment/jSmugmugBackup/
 jar -cvfe ../jSmugmugBackup.jar jSmugmugBackup.main.Main jSmugmugBackup/ org/ documentation/
 cd ../..
 
+#copy license info
+mkdir deployment/docs
+cp -v docs/LICENSE.txt deployment/docs/LICENSE.jSmugmugBackup.txt
+cp -v lib/httpcomponents-client-4.0-beta1/LICENSE.txt deployment/docs/LICENSE.apache-httpcomponents-client.txt
+cp -v lib/httpcomponents-client-4.0-beta1/NOTICE.txt deployment/docs/NOTICE.apache-httpcomponents-client.txt
+cp -v lib/httpcomponents-client-4.0-beta1/README.txt deployment/docs/README.apache-httpcomponents-client.txt
+cp -v lib/json_simple/LICENSE.txt deployment/docs/LICENSE.json_simple.txt
+cp -v lib/json_simple/AUTHORS.txt deployment/docs/AUTHORS.json_simple.txt
+cp -v lib/json_simple/README.txt deployment/docs/README.json_simple.txt
 
 #cleanup
 rm -r -f deployment/jSmugmugBackup
@@ -61,3 +70,9 @@ chmod -v u+x deployment/jSmugmugBackup.sh
 #echo "@set CLASSPATH=\".:lib\\commons-codec-1.3.jar:lib\\commons-httpclient-3.1-rc1.jar:lib\\commons-io-1.3.2.jar:lib\\commons-logging-1.1.1.jar:lib\\json-1.0.0.jar:lib\\junit-4.4.jar:lib\\logback-classic-0.9.8.jar:lib\\logback-core-0.9.8.jar:lib\\migbase64-2.2.0.jar:lib\\slf4j-api-1.4.3.jar:lib\\db-derby-10.3.1.4-bin\\derby.jar:lib\\db-derby-10.3.1.4-bin\\derbyLocale_de_DE.jar:lib\\db-derby-10.3.1.4-bin\\derbyclient.jar:lib\\db-derby-10.3.1.4-bin\\derbynet.jar:lib\\db-derby-10.3.1.4-bin\\derbyrun.jar:lib\\db-derby-10.3.1.4-bin\\derbytools.jar\"" >> deployment/jSmugmugBackup/jSmugmugBackup.bat
 #echo "@java -classpath %CLASSPATH% -jar jSmugmugBackup.jar %1 %2 %3 %4 %5 %6 %7 %8 %9" >> deployment/jSmugmugBackup/jSmugmugBackup.bat
 echo "@java -Xms512m -Xmx1024m -jar jSmugmugBackup.jar %1 %2 %3 %4 %5 %6 %7 %8 %9" >> deployment/jSmugmugBackup.bat
+
+
+#creat archive
+cd deployment
+tar cvjf jSmugmugBackup.tar.bz2 *
+cd ..
