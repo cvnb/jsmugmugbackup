d63d77a37d58b6b88f5a5ad042d8308ae259bfd6
chradam <chradam@5eeee95c-e655-0410-a769-073df2a2cce5>
2008-12-11 10:39:13 +0000

    added missing files to repository

---
+++ trunk/jSmugmugBackup/src/jSmugmugBackup/model/accountLayer/RootElement.java
@@ -0,0 +1,36 @@
+/*
+ * Created on Nov 27, 2008
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package jSmugmugBackup.model.accountLayer;
+
+import java.util.Vector;
+
+public class RootElement extends SmugmugObject implements IRootElement
+{
+	private Vector<ICategory> categoryList = null;
+
+	public RootElement(String name)
+	{
+		super(null, 0, name);
+		
+		this.categoryList = new Vector<ICategory>();
+	}
+
+	public SmugmugTypeEnum getSmugmugType() { return SmugmugTypeEnum.SMUGMUG_ROOT; }
+
+	
+	public void addCategory(ICategory category)
+	{
+		this.categoryList.add(category);
+	}
+	
+	public Vector<ICategory> getCategoryList()
+	{
+		return this.categoryList;
+	}
+
+
+}
---
+++ trunk/jSmugmugBackup/src/jSmugmugBackup/model/accountLayer/SmugmugTypeEnum.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Nov 27, 2008
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package jSmugmugBackup.model.accountLayer;
+
+public enum SmugmugTypeEnum
+{
+	//SMUGMUG_OBJECT,
+	SMUGMUG_IMAGE,
+	SMUGMUG_ALBUM,
+	SMUGMUG_SUBCATEGORY,
+	SMUGMUG_CATEGORY,
+	SMUGMUG_ROOT
+}
