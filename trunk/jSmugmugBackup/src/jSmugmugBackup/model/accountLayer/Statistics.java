125c6fb48036691c36071d627f99940a59ece2b9
chradam <chradam@5eeee95c-e655-0410-a769-073df2a2cce5>
2009-04-25 02:29:13 +0000

    refactoring: connector.getTree()

---
+++ trunk/jSmugmugBackup/src/jSmugmugBackup/model/accountLayer/Statistics.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jSmugmugBackup.model.accountLayer;
+
+/**
+ *
+ * @author paul
+ */
+public class Statistics
+{
+    // encapsulation statistics data into an object, so we can with references
+
+    public int estimatedImageCount = 0;
+    public int estimatedAlbumCount = 0;
+
+
+    public final double completionStep = 0.1;
+    public double currCompletionStep = completionStep;
+    public int imageCount = 0;
+
+}
