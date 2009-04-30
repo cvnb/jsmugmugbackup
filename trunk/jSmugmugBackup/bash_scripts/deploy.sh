fc627ccd23a8ba0083362cafa7ffab9b4d97b7fb
chradam <chradam@5eeee95c-e655-0410-a769-073df2a2cce5>
2009-04-30 18:47:57 +0000

    added script to delete all cache files

--- trunk/jSmugmugBackup/bash_scripts/deploy.sh
+++ trunk/jSmugmugBackup/bash_scripts/deploy.sh
@@ -73,6 +73,11 @@ echo "#!/bin/bash" >> deployment/jSmugmugBackup.sh
 echo "java -Xms512m -Xmx1024m -jar jSmugmugBackup.jar \$1 \$2 \$3 \$4 \$5 \$6 \$7 \$8 \$9" >> deployment/jSmugmugBackup.sh
 chmod -v u+x deployment/jSmugmugBackup.sh
 
+#create unix clear cache script
+echo "#!/bin/bash" >> deployment/clearCache.sh
+echo "rm -v jSmugmugBackup.albumCache.*" >> deployment/clearCache.sh
+chmod -v u+x deployment/clearCache.sh
+
 #create windows .bat
 #echo "@set CLASSPATH=\".:lib\\commons-codec-1.3.jar:lib\\commons-httpclient-3.1-rc1.jar:lib\\commons-io-1.3.2.jar:lib\\commons-logging-1.1.1.jar:lib\\json-1.0.0.jar:lib\\junit-4.4.jar:lib\\logback-classic-0.9.8.jar:lib\\logback-core-0.9.8.jar:lib\\migbase64-2.2.0.jar:lib\\slf4j-api-1.4.3.jar:lib\\db-derby-10.3.1.4-bin\\derby.jar:lib\\db-derby-10.3.1.4-bin\\derbyLocale_de_DE.jar:lib\\db-derby-10.3.1.4-bin\\derbyclient.jar:lib\\db-derby-10.3.1.4-bin\\derbynet.jar:lib\\db-derby-10.3.1.4-bin\\derbyrun.jar:lib\\db-derby-10.3.1.4-bin\\derbytools.jar\"" >> deployment/jSmugmugBackup/jSmugmugBackup.bat
 #echo "@java -classpath %CLASSPATH% -jar jSmugmugBackup.jar %1 %2 %3 %4 %5 %6 %7 %8 %9" >> deployment/jSmugmugBackup/jSmugmugBackup.bat
